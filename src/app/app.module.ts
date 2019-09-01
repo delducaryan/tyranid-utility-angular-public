19081f14c7fe12607b9f8cad38552e1157873f32
Ryan Laptop <delduca.ryan@gmail.com>
2019-09-01 16:03:55 -0700

    Configured AngularFire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
@@ -11,8 +13,10 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/firestore.service.ts
+++ src/app/firestore.service.ts
@@ -1,26 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  Action,
-  AngularFirestore,
-  AngularFirestoreCollection,
-  AngularFirestoreDocument,
-  DocumentChangeAction,
-  DocumentSnapshotDoesNotExist,
-  DocumentSnapshotExists,
-} from 'angularfire2/firestore';
-import {
-  from,
-  Observable,
-} from 'rxjs';
-import {
-  expand,
-  map,
-  mergeMap,
-  switchMap,
-  take,
-  takeWhile,
-  tap,
-} from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
