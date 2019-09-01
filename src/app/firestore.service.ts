446e877ffd28c2f3b264738196c4b7bac15c48f7
Ryan Laptop <delduca.ryan@gmail.com>
2019-09-01 15:34:36 -0700

    Added AngularFire2 and Firebase package. Configured Firebase project. Added Firestore service

--- src/app/firestore.service.ts
+++ src/app/firestore.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {
+  Action,
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+  DocumentChangeAction,
+  DocumentSnapshotDoesNotExist,
+  DocumentSnapshotExists,
+} from 'angularfire2/firestore';
+import {
+  from,
+  Observable,
+} from 'rxjs';
+import {
+  expand,
+  map,
+  mergeMap,
+  switchMap,
+  take,
+  takeWhile,
+  tap,
+} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
 }
