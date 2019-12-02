c7e3a666efafbef620e6cc04682e5d8cd64bdd71
Ryan Laptop <delduca.ryan@gmail.com>
2019-12-02 13:47:20 -0800

    First deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCgDr36bKuqbcTmDsv2iZC31_XQuvBoziY',
+    authDomain: 'tyranid-utility-angular.firebaseapp.com',
+    databaseURL: 'https://tyranid-utility-angular.firebaseio.com',
+    projectId: 'tyranid-utility-angular',
+    storageBucket: 'tyranid-utility-angular.appspot.com',
+    messagingSenderId: '40199156419',
+    appId: '1:40199156419:web:f5e90ed4aba8c6cf',
+  },
   production: true
 };
